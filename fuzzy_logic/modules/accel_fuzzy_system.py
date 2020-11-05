f430bb399105e9822bc76f64801df991f7940ab4
dspiljak <dominik.spiljak@reversinglabs.com>
2020-11-05 20:56:10 +0100

    Finished boat simulator

--- fuzzy_logic/modules/accel_fuzzy_system.py
+++ fuzzy_logic/modules/accel_fuzzy_system.py
@@ -6,6 +6,7 @@ from domain import Domain
 from domain_element import DomainElement
 from operations import Operations
 from int_unaryfunction import IntUnaryFunction
+from functools import reduce
 import abc
 
 
@@ -13,75 +14,82 @@ class AccelFuzzySystem(FuzzySystem):
 
     def __init__(self, defuzzier):
         super().__init__(defuzzier)
-        domain = Domain.intRange(0, 1301)
 
-        self.LKDK_distances = [
-            # Jako blizu
-            CalculatedFuzzySet(
-                domain, StandardFuzzySets.lFunction(25, 35)),
-            # Dosta blizu
-            CalculatedFuzzySet(
-                domain, StandardFuzzySets.lambdaFunction(30, 40, 50)),
-            # Relativno blizu
-            CalculatedFuzzySet(
-                domain, StandardFuzzySets.lambdaFunction(40, 60, 80)),
-            # Daleko
-            CalculatedFuzzySet(
-                domain, StandardFuzzySets.gammaFunction(70, 120))
-        ]
+        self.velocity_domain = Domain.intRange(0, 1300)
+        self.acceleration_domain = Domain.intRange(-35, 35)
+        self.distance_domain = Domain.intRange(0, 1301)
 
-        self.LD_distances = [
-            # Jako blizu
-            CalculatedFuzzySet(
-                domain, StandardFuzzySets.lFunction(20, 25)),
-            # Dosta blizu
-            CalculatedFuzzySet(
-                domain, StandardFuzzySets.lambdaFunction(20, 25, 30)),
-            # Relativno blizu
-            CalculatedFuzzySet(
-                domain, StandardFuzzySets.lambdaFunction(25, 40, 55)),
-            # Daleko
-            CalculatedFuzzySet(
-                domain, StandardFuzzySets.gammaFunction(50, 120))
-        ]
+        self.velocity = [
+            # Sporo
+            CalculatedFuzzySet(self.velocity_domain,
+                               StandardFuzzySets.lFunction(40, 75)),
 
-        speed_domain = Domain.intRange(0, 1301)
-        self.speed_values = [
-            # Jako sporo
-            CalculatedFuzzySet(
-                speed_domain, StandardFuzzySets.lFunction(5, 10)),
-            # Dosta sporo
-            CalculatedFuzzySet(
-                speed_domain, StandardFuzzySets.lambdaFunction(5, 15, 25)),
             # Normalno
-            CalculatedFuzzySet(
-                speed_domain, StandardFuzzySets.lambdaFunction(20, 30, 40)),
-            # Dosta brzo
-            CalculatedFuzzySet(
-                speed_domain, StandardFuzzySets.lambdaFunction(30, 40, 50)),
-            # Jako brzo
-            CalculatedFuzzySet(
-                speed_domain, StandardFuzzySets.gammaFunction(40, 60))
+            CalculatedFuzzySet(self.velocity_domain,
+                               StandardFuzzySets.lambdaFunction(60, 75, 90)),
+
+            # Brzo
+            CalculatedFuzzySet(self.velocity_domain,
+                               StandardFuzzySets.gammaFunction(75, 90))
         ]
 
-        accel_domain = Domain.intRange(-50, 50)
-        self.accel_values = [
-            # Jako uspori
-            CalculatedFuzzySet(
-                accel_domain, StandardFuzzySets.lFunction(-20, -10)),
-            # Slabo uspori
-            CalculatedFuzzySet(
-                accel_domain, StandardFuzzySets.lambdaFunction(-15, -7, 0)),
-            # Neutralno
-            CalculatedFuzzySet(
-                accel_domain, StandardFuzzySets.lambdaFunction(-5, 0, 5)),
-            # Slabo ubrzaj
-            CalculatedFuzzySet(
-                accel_domain, StandardFuzzySets.lambdaFunction(0, 7, 15)),
-            # Jako ubrzaj
-            CalculatedFuzzySet(
-                accel_domain, StandardFuzzySets.gammaFunction(10, 20))
+        self.distance = [
+            # Jako Blizu
+            CalculatedFuzzySet(self.distance_domain,
+                               StandardFuzzySets.lFunction(15, 30)),
+            # Blizu
+            CalculatedFuzzySet(self.distance_domain,
+                               StandardFuzzySets.lFunction(30, 45))
         ]
+
+        self.acceleration = [
+            # Uspori
+            CalculatedFuzzySet(self.acceleration_domain,
+                               StandardFuzzySets.lFunction(-20, -10)),
+            # Slabo uspori
+            CalculatedFuzzySet(self.acceleration_domain,
+                               StandardFuzzySets.lambdaFunction(-15, -10, 0)),
+            # Slabo ubrzaj
+            CalculatedFuzzySet(self.acceleration_domain,
+                               StandardFuzzySets.lambdaFunction(0, 10, 15)),
+            # Ubrzaj
+            CalculatedFuzzySet(self.acceleration_domain,
+                               StandardFuzzySets.gammaFunction(10, 20)),
+        ]
+
+        self.rules = [
+            # [L, D, LK, DK, V, S]
+            [[None, None, None, None, self.velocity[0], None], self.acceleration[3]],
+            [[None, None, None, None, self.velocity[2], None], self.acceleration[0]],
+            [[None, None, self.distance[1], None, None, None], self.acceleration[0]],
+            [[None, None, None, self.distance[1], None, None], self.acceleration[0]],
+            [[self.distance[0], None, None, None, None, None], self.acceleration[3]],
+            [[None, self.distance[0], None, None, None, None], self.acceleration[3]],
+        ]
+
+    def conclude_rule(self, singletons, rule, and_operator, or_operator):
+
+        premises = rule[0]
+        y = rule[1]
+
+        premises_values = []
+        for singleton, premise in zip(singletons, premises):
+            if premise is None:
+                premises_values.append(1)
+            elif type(premise) is list:
+                premises_values.append(or_operator(
+                    [p.getValueAt(singleton) for p in premise]))
+            else:
+                premises_values.append(premise.getValueAt(singleton))
+
+        value = and_operator(premises_values)
+
+        # Pripadnost akcedenta
+        cutoff = CalculatedFuzzySet(
+            y.getDomain(), IntUnaryFunction(lambda x: value))
+
+        return Operations.binaryOperation(
+            cutoff, y, Operations.zadehAnd())
 
     @ abc.abstractclassmethod
     def conclude(self, L, D, LK, DK, V, S):
@@ -92,72 +100,54 @@ class AccelFuzzySystemMin(AccelFuzzySystem):
 
     def __init__(self, defuzzier):
         super().__init__(defuzzier)
-        self.rules = [
-            # Ako smo jako blizu za LK i idemo jako brzo, jako uspori
-            # Ako smo jako blizu za LK i idemo dosta brzo, jako uspori
-            [self.LKDK_distances[0], self.speed_values[4], self.accel_values[0]],
-            [self.LKDK_distances[0], self.speed_values[3], self.accel_values[0]],
-            # Ako smo dosta blizu LK i idemo jako brzo, slabo uspori
-            # Ako smo dosta blizu LK i idemo jako sporo, slabo ubrzaj
-            # Ako smo dosta blizu LK i idemo dosta sporo, slabo ubrzaj
-            [self.LKDK_distances[1], self.speed_values[4], self.accel_values[1]],
-            [self.LKDK_distances[1], self.speed_values[0], self.accel_values[3]],
-            # Ako smo relativno blizu Lk i idemo jako sporo, jako ubrzaj
-            # Ako smo relativno blizu Lk i idemo dosta sporo, slabo ubrzaj
-            [self.LKDK_distances[2], self.speed_values[0], self.accel_values[3]],
-            [self.LKDK_distances[2], self.speed_values[1], self.accel_values[4]],
-            # Ako smo daleko od Lk i idemo jako sporo, jako ubrzaj
-            # Ako smo daleko od Lk i idemo dosta sporo, jako ubrzaj
-            # Ako smo daleko od Lk i idemo dosta sporo, slabo ubrzaj
-            [self.LKDK_distances[3], self.speed_values[0], self.accel_values[4]],
-            [self.LKDK_distances[3], self.speed_values[1], self.accel_values[4]],
-
-            # Isto za i DK
-            # Ako smo jako blizu za DK i idemo jako brzo, jako uspori
-            # Ako smo jako blizu za DK i idemo dosta brzo, jako uspori
-            [self.LKDK_distances[0], self.speed_values[4], self.accel_values[0]],
-            [self.LKDK_distances[0], self.speed_values[3], self.accel_values[0]],
-            # Ako smo dosta blizu DK i idemo jako brzo, slabo uspori
-            # Ako smo dosta blizu DK i idemo jako sporo, slabo ubrzaj
-            [self.LKDK_distances[1], self.speed_values[4], self.accel_values[1]],
-            [self.LKDK_distances[1], self.speed_values[0], self.accel_values[3]],
-            # Ako smo relativno blizu DK i idemo jako sporo, jako ubrzaj
-            # Ako smo relativno blizu DK i idemo dosta sporo, slabo ubrzaj
-            [self.LKDK_distances[2], self.speed_values[0], self.accel_values[3]],
-            [self.LKDK_distances[2], self.speed_values[1], self.accel_values[4]],
-            # Ako smo daleko od DK i idemo jako sporo, jako ubrzaj
-            # Ako smo daleko od DK i idemo dosta sporo, jako ubrzaj
-            [self.LKDK_distances[3], self.speed_values[0], self.accel_values[4]],
-            [self.LKDK_distances[3], self.speed_values[1], self.accel_values[4]],
-        ]
+        self.and_operator = min
+        self.or_operator = max
 
     def conclude(self, L, D, LK, DK, V, S):
-        LK_element = DomainElement.of(LK)
-        DK_element = DomainElement.of(DK)
-        V_element = DomainElement.of(V)
-        L_element = DomainElement.of(L)
-        D_element = DomainElement.of(D)
+
+        singletons = list(map(DomainElement.of, [L, D, LK, DK, V, S]))
 
         # Prazan fuzzy_set
-        conclusion = MutableFuzzySet(self.rules[0][2].getDomain())
+        conclusion = MutableFuzzySet(self.acceleration_domain)
 
-        for i, rule in enumerate(self.rules):
-            if i < 8:
-                value = min([rule[0].getValueAt(LK_element),
-                             rule[1].getValueAt(LK_element)])
-            else:
-                value = min([rule[0].getValueAt(DK_element),
-                             rule[1].getValueAt(V_element)])
-
+        for rule in self.rules:
             # Pripadnost akcedenta
-            cutoff = CalculatedFuzzySet(
-                rule[2].getDomain(), IntUnaryFunction(lambda x: value))
-
-            # Get y with cutoff
-            y_cutoff = Operations.binaryOperation(
-                cutoff, rule[2], Operations.zadehAnd())
+            y_cutoff = self.conclude_rule(
+                singletons, rule, self.and_operator, self.or_operator)
 
             conclusion = Operations.binaryOperation(
                 conclusion, y_cutoff, Operations.zadehOr())
 
         return int(self.defuzzier.decode(conclusion))
+
+
+class AccelFuzzySystemProduct(AccelFuzzySystem):
+
+    def __init__(self, defuzzier):
+        super().__init__(defuzzier)
+        self.and_operator = min
+        self.or_operator = lambda l: reduce(lambda x, y: x * y, l)
+
+    def conclude(self, L, D, LK, DK, V, S):
+
+        singletons = list(map(DomainElement.of, [L, D, LK, DK, V, S]))
+
+        # Prazan fuzzy_set
+        conclusion = MutableFuzzySet(self.acceleration_domain)
+
+        for rule in self.rules:
+            # Pripadnost akcedenta
+            y_cutoff = self.conclude_rule(
+                singletons, rule, self.and_operator, self.or_operator)
+
+            conclusion = Operations.binaryOperation(
+                conclusion, y_cutoff, Operations.zadehOr())
+
+        return int(self.defuzzier.decode(conclusion))
+
+
+if __name__ == "__main__":
+    from defuzziers import COADefuzzifier
+    defuz = COADefuzzifier()
+    sys = AccelFuzzySystemMin(defuz)
+    print(sys.conclude(L=20, D=50, LK=40, DK=60, V=0, S=1))
