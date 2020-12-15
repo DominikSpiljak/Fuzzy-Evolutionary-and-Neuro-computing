a3231edca5ef257942b1a4b807ed07028abb87ea
dspiljak <dominik.spiljak@reversinglabs.com>
2020-12-15 21:35:16 +0100

    WIP parameter updating

--- ANFIS/fuzzy_net.py
+++ ANFIS/fuzzy_net.py
@@ -4,49 +4,55 @@ import pickle
 
 class ANFIS():
 
-    def __init__(self, no_variables=None, no_rules=None):
+    def __init__(self, no_variables=2, no_rules=None):
         if no_variables is not None and no_rules is not None:
-            self.pq = np.random.randn(no_rules, no_variables)
+            self.p = np.random.randn(no_rules, 1)
+            self.q = np.random.randn(no_rules, 1)
             self.r = np.zeros((no_rules, 1))
 
             self.a = np.random.randn(no_rules, no_variables)
             self.b = np.random.randn(no_rules, no_variables)
 
-    def fuzzification_sigmoid(self, X):
-        return 1 / (1 + np.exp(self.b * (X - self.a)))
+    def fuzzification_sigmoid(self, x_y):
+        return 1 / (1 + np.exp(self.b * (x_y - self.a)))
 
-    def mean_squared_err(self, y_true, preds):
-        return np.mean(np.square(y_true - preds))
+    def mean_squared_err(self, z_true, preds):
+        return np.mean(np.square(z_true - preds))
 
-    def forward(self, x, underflow_ctrl=1e-12):
+    def forward(self, X_Y, underflow_ctrl=1e-12):
+        outs = []
+        alphas = []
 
-        # 1. layer: Fuzzification
-        fuzzified = self.fuzzification_sigmoid(x)
+        for x_y in X_Y:
 
-        # 2. layer: Product of inputs
-        products = np.product(fuzzified, axis=1)
+            # 1. layer: Fuzzification
+            fuzzified = self.fuzzification_sigmoid(x_y)
 
-        # 3. layer: Normalization
-        normalized = (products / (underflow_ctrl if np.sum(products)
-                                  == 0 else np.sum(products)))
+            # 2. layer: Product of inputs
+            products = np.product(fuzzified, axis=1)
 
-        # 4. layer: Defuzzification (w * (p * x1 + q * x2 + r))
-        deffuzified = normalized.dot(
-            np.sum(np.multiply(self.pq, x), axis=1) + self.r)
+            alphas.append(products)
 
-        # 5. layer: Output layer (sum(wi * fi) / sum(w))
-        out = (np.sum(deffuzified) / (underflow_ctrl if np.sum(normalized)
-                                      == 0 else np.sum(normalized)))
+            # 3. layer: Normalization
+            normalized = (products / (underflow_ctrl if np.sum(products)
+                                      == 0 else np.sum(products)))
 
-        return np.array([out]), np.array(normalized)
+            x, y = x_y.T
 
-    def train(self, X, y, learning_rate=1e-3, n_epochs=10000, algorithm='backprop', underflow_ctrl=1e-12, shuffle=True):
+            # 4. layer: Defuzzification (w * (p * x1 + q * x2 + r))
+            deffuzified = normalized.dot(self.p * x + self.q * y + self.r)
 
-        if algorithm not in ['backprop', 'stohastic']:
+            outs.append(deffuzified)
+
+        return np.array(outs), np.array(alphas)
+
+    def train(self, X_Y, z, learning_rate=1e-3, n_epochs=10000, algorithm='backprop', batch_size=10, underflow_ctrl=1e-12, shuffle=True):
+
+        if algorithm not in ['backprop', 'stohastic', 'minibatch']:
             raise ValueError('Algorithm not recognised')
 
         if algorithm == 'backprop':
-            batch_size = X.shape[0]
+            batch_size = X_Y.shape[0]
 
         elif algorithm == 'stohastic':
             batch_size = 1
@@ -54,43 +60,46 @@ class ANFIS():
         for i in range(n_epochs):
 
             if i % 100 == 0:
-                preds = self.predict(X, underflow_ctrl=underflow_ctrl)
+                preds = self.predict(X_Y, underflow_ctrl=underflow_ctrl)
                 print('Iteration {}, error {}'.format(
-                    i, self.mean_squared_err(y, preds)))
+                    i, self.mean_squared_err(z, preds)))
 
             if shuffle:
-                indices = np.random.permutation(X.shape[0])
+                indices = np.random.permutation(X_Y.shape[0])
             else:
-                indices = np.arange(X.shape[0])
+                indices = np.arange(X_Y.shape[0])
 
-            for j in range(0, X.shape[0], batch_size):
+            for j in range(0, X_Y.shape[0], batch_size):
+                weight_errs = []
+
                 sliced_indices = indices[j:j + batch_size]
 
-                X_batch = X[sliced_indices]
+                preds, alphas = self.forward(
+                    X_Y[sliced_indices], underflow_ctrl=underflow_ctrl)
 
-                batch_preds = []
-                batch_alphas = []
+            x, y = X_Y[sliced_indices].T
 
-                for x in X_batch:
-                    preds, alphas = self.forward(
-                        x, underflow_ctrl=underflow_ctrl)
-                    batch_preds.append(preds)
-                    batch_alphas.append(alphas)
+            x = x.reshape(x.shape[0], 1)
+            y = y.reshape(y.shape[0], 1)
 
-                batch_preds = np.array()
+            Ek_pi = - 1 * (z - preds) * \
+                ((x.T.dot(alphas)) / np.sum(alphas, axis=0))
 
-                dEk_dpi = -()
+            Ek_qi = - 1 * (z - preds) * \
+                ((y.T.dot(alphas)) / np.sum(alphas, axis=0))
 
-    def predict(self, X, underflow_ctrl=1e-12):
-        all_preds = []
-        for x in X:
-            preds, _ = self.forward(x, underflow_ctrl=underflow_ctrl)
-            all_preds.append(preds)
-        return np.array(all_preds)
+            Ek_ri = - 1 * (z - preds) * \
+                ((alphas) / np.sum(alphas, axis=0))
+
+            # TODO: fix dimensions
+
+    def predict(self, X_Y, underflow_ctrl=1e-12):
+        preds, _ = self.forward(X_Y, underflow_ctrl=underflow_ctrl)
+        return preds
 
 
 if __name__ == "__main__":
     anfis = ANFIS(2, 2)
-    X = np.array([[0, 0], [1, 1], [2, 2], [3, 3]])
-    y = np.array([[0], [2], [4], [6]])
-    anfis.train(X, y, algorithm='backprop', n_epochs=1)
+    X_Y = [[0, 0], [1, 1], [2, 2], [3, 3]]
+    z = [[0], [2], [4], [4]]
+    anfis.train(np.array(X_Y), np.array(z), n_epochs=1, shuffle=False)
