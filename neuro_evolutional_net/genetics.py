Fixed neural net calculation

neuro_evolutional_net/genetics.py
@@ -1,9 +1,10 @@
 import numpy as np
 from individual import Individual
 
+
 def generate_population(no_params, population_size, neural_net):
     def generator():
-        return [Individual(np.random.rand(no_params) * 2 - 1, neural_net) for _ in range(population_size)]
+        return [Individual(np.random.rand(no_params), neural_net) for _ in range(population_size)]
     return generator
 
 
@@ -15,7 +16,7 @@ def tournament_selection(k=3):
         comb_population.append([selected_sorted[0], selected_sorted[1]])
         population.remove(selected_sorted[k-1])
         return population, comb_population
-    
+
     return selector
 
 
@@ -24,7 +25,8 @@ def weight_recombination(neural_net):
         children = []
         for parents in comb_population:
             child_vals = []
-            rand= (np.random.rand(neural_net.get_num_params()) >= 0.5).astype('int')
+            rand = (np.random.rand(neural_net.get_num_params())
+                    >= 0.5).astype('int')
             for i in range(neural_net.get_num_params()):
                 child_vals.append(parents[rand[i]].get_value()[i])
             children.append(Individual(child_vals, neural_net))
@@ -37,7 +39,8 @@ def simulated_binary_recombination(neural_net):
         children = []
         for parents in comb_population:
             rand = np.random.rand(neural_net.get_num_params())
-            child_vals = (1 - rand) * parents[0].get_value() + rand * parents[1].get_value()
+            child_vals = (1 - rand) * \
+                parents[0].get_value() + rand * parents[1].get_value()
             children.append(Individual(child_vals, neural_net))
         return children
     return recombinator
@@ -60,25 +63,37 @@ def cross_chooser(cross_list):
     return cross
 
 
-def mutation_1(mutation_prob, deviation):
+def mutation_1(mutation_prob, deviation, val_range=None):
     def mutator(children):
         for child in children:
             rand = np.random.rand(len(child.get_value()))
             for i in range(len(child.get_value())):
                 if rand[i] < mutation_prob:
                     child.value[i] += np.random.normal(0, scale=deviation)
+                    if val_range is not None:
+                        if child.value[i] < val_range[0]:
+                            child.value[i] = val_range[0]
+                        elif child.value[i] > val_range[1]:
+                            child.value[i] = val_range[1]
         return children
     return mutator
 
-def mutation_2(mutation_prob, deviation):
+
+def mutation_2(mutation_prob, deviation, val_range=None):
     def mutator(children):
         for child in children:
             rand = np.random.rand(len(child.get_value()))
             for i in range(len(child.get_value())):
                 if rand[i] < mutation_prob:
                     child.value[i] = np.random.normal(0, scale=deviation)
+                    if val_range is not None:
+                        if child.value[i] < val_range[0]:
+                            child.value[i] = val_range[0]
+                        elif child.value[i] > val_range[1]:
+                            child.value[i] = val_range[1]
         return children
     return mutator
+
 
 def mutation_chooser(mutation_list, probs, neural_net):
     probs = np.array(probs)
@@ -88,8 +103,9 @@ def mutation_chooser(mutation_list, probs, neural_net):
         mutation = np.random.choice(mutation_list, p=probs)
         mutated = mutation(children)
         for c in mutated:
-            c.error = neural_net.calculate_error(c.value)
-        
+            c.error = neural_net.calculate_error(
+                [c.w_type_1, c.s_type_1, c.w, c.b])
+
         return mutated
 
     return mutation_func
@@ -101,4 +117,3 @@ def solution():
             population, key=lambda individual: individual.error)
         return sorted_population[0]
     return solution_func
-

neuro_evolutional_net/individual.py
@@ -2,11 +2,15 @@ import numpy as np
 import pickle
 from copy import deepcopy
 
+
 class Individual:
 
     def __init__(self, value, neural_net):
         self.value = np.array(value.copy())
-        self.error = neural_net.calculate_error(self.value)
+        self.w_type_1, self.s_type_1, self.w, self.b = neural_net.decode_params(
+            self.value)
+        self.error = neural_net.calculate_error(
+            [self.w_type_1, self.s_type_1, self.w, self.b])
 
     def get_value(self):
         return self.value.copy()

neuro_evolutional_net/neural_net.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class NeuralNet:
 
     def __init__(self, layers, dataset):
@@ -14,8 +15,9 @@ class NeuralNet:
         no_neuron_type1_weights = self.layers[1] * self.layers[0]
         no_neuron_type1_s = self.layers[1] * self.layers[0]
 
-        self.no_params = no_weights + no_biases + no_neuron_type1_weights + no_neuron_type1_s
-    
+        self.no_params = no_weights + no_biases + \
+            no_neuron_type1_weights + no_neuron_type1_s
+
     def get_num_params(self):
         return self.no_params
 
@@ -23,39 +25,40 @@ class NeuralNet:
     def sigmoid(X):
         return 1 / (1 + np.exp(-X))
 
-    def forward(self, params, X):
+    def forward(self, w_type_1, s_type_1, w, b, X):
         layer_outputs = []
         for i, layer in enumerate(self.layers):
             if i == 0:
                 continue
             elif i == 1:
                 for j in range(layer):
-                    neuron_params = params[j * 4:j * 4 + 4]
-                    layer_outputs.append(np.sqrt(np.sum(np.square(
-                                    X - np.array(neuron_params[:2])), axis=1)) / np.sqrt(np.sum(np.square(np.array(neuron_params[2:])))))
-                layer_outputs = np.array(layer_outputs)
-                layer_outputs = layer_outputs.reshape(layer_outputs.shape[1], layer_outputs.shape[0])
-                last_idx = (layer - 1) * 4 + 4
-            else:
-                weights = np.array(params[last_idx:last_idx + self.layers[i - 1] * self.layers[i]]).reshape(self.layers[i - 1], self.layers[i])
-                last_idx = last_idx + self.layers[i - 1] * self.layers[i]
-                biases = np.array(params[last_idx:last_idx + self.layers[i]])
-                last_idx = last_idx + self.layers[i]
+                    layer_outputs.append(
+                        1 / (1 + np.sqrt(np.sum(np.square((X - w_type_1[j].flatten()) / s_type_1[j].flatten()), axis=1))))
+                layer_outputs = np.array(layer_outputs).T
 
-                layer_outputs = NeuralNet.sigmoid(layer_outputs.dot(weights) + biases)
-        
+            else:
+                weights = w[i - 2]
+                biases = b[i - 2]
+
+                layer_outputs = NeuralNet.sigmoid(
+                    layer_outputs.dot(weights) + biases)
+
         return layer_outputs
 
-
     def calculate_error(self, params):
-        return np.mean(np.sum(np.square(self.forward(params, self.dataset.X) - self.dataset.y), axis=1))
-                
+        w_type_1, s_type_1, w, b = params
+        return np.mean(np.sum(np.square(self.forward(w_type_1, s_type_1, w, b, self.dataset.X) - self.dataset.y), axis=1))
+
     def predict(self, params, X):
-        return np.argmax(self.forward(params, X), axis=1)
+        w_type_1, s_type_1, w, b = params
+        return np.argmax(self.forward(w_type_1, s_type_1, w, b, X), axis=1)
 
     def decode_params(self, params):
         w_type_1 = []
         s_type_1 = []
+
+        weights = []
+        biases = []
 
         last_idx = 0
 
@@ -68,17 +71,17 @@ class NeuralNet:
                     s_type_1.append(params[last_idx + 2: last_idx + 4])
                     last_idx = last_idx + 4
             else:
-                w = np.array(params[last_idx:last_idx + self.layers[i - 1] * self.layers[i]]).reshape(self.layers[i - 1], self.layers[i])
+                w = np.array(params[last_idx:last_idx + self.layers[i - 1] *
+                                    self.layers[i]]).reshape(self.layers[i - 1], self.layers[i])
+                weights.append(w)
                 last_idx = last_idx + self.layers[i - 1] * self.layers[i]
                 b = np.array(params[last_idx:last_idx + self.layers[i]])
+                biases.append(b)
                 last_idx = last_idx + self.layers[i]
 
-        return np.array(w_type_1), np.array(s_type_1), np.array(w), np.array(b)
-
+        return np.array(w_type_1), np.array(s_type_1), np.array(weights), np.array(biases)
 
     def show(self, params, save_file=None):
-        
-        # TODO: implement weights drawing
 
         ax = plt.figure(figsize=(12, 12)).gca()
         left = .1
@@ -103,7 +106,7 @@ class NeuralNet:
             for m in range(layer_size_a):
                 for o in range(layer_size_b):
                     line = plt.Line2D([n*h_spacing + left, (n + 1)*h_spacing + left],
-                                    [layer_top_a - m*v_spacing, layer_top_b - o*v_spacing], c='k')
+                                      [layer_top_a - m*v_spacing, layer_top_b - o*v_spacing], c='k')
                     ax.add_artist(line)
         ax.get_xaxis().set_visible(False)
         ax.get_yaxis().set_visible(False)
@@ -112,4 +115,3 @@ class NeuralNet:
             plt.savefig(save_file)
 
         plt.show()
-        